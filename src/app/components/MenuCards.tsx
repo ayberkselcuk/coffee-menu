8ca1d3a42db6c9f6ae8941174f6edf937518c414
Ayberk Selçuk <selcukayberk1@gmail.com>
2024-02-02 23:37:57 +0300

    project added

---
+++ src/app/components/MenuCards.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "next/image";
+
+
+interface MenuCardsProps {
+    title: string;
+    ingredients: string[];
+    description: string;
+    image: string;
+}
+
+const MenuCards = (props: MenuCardsProps) => {
+    return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+            <Image
+                className="w-full"
+                width={200}
+                height={200}
+                src={props.image}
+                alt={props.title}
+            />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2 text-red-500 dark:text-red-500">
+                    {props.title}
+                </div>
+                <p className="text-black dark:text-white text-base">
+                    {props.description}
+                </p>
+                <br></br>
+            </div>
+        </div>
+    );
+};
+
+export default MenuCards;
